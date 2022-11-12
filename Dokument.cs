b71dd99bafd057e9ee5a4b3e10229d7421db859e
k s <klaus.stolz@web.de>
2022-11-12 13:57:48 +0100

    Add project files.

---
+++ Dokument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBeispiel {
+    class Dokument {
+        private string name;
+
+        public void SetName ( string n ) {
+            name = n;
+        }
+
+        public string GetName ( ) {
+            return name;
+        }
+    }
+}
---
+++ WordDokument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBeispiel {
+    class WordDokument : Dokument, IDruckbar {
+        public void Drucken ( ) {
+            Console.WriteLine ( "Word" );
+        }
+
+        public void Erinnern ( ) {
+
+        }
+    }
+}
